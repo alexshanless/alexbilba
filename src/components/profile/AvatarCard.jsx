ad57a14e90342b86ff41ba6bd126ad69ed6ce398
Alex Bilba <alex.shanless@gmail.com>
2025-11-14 18:15:34 -0800

    Update AvatarCard.jsx

--- src/components/profile/AvatarCard.jsx
+++ src/components/profile/AvatarCard.jsx
@@ -3,7 +3,7 @@ const AvatarCard = () => {
     <div className='avatar-card'>
       <div className='avatar-placeholder'>
         <div className='profile-picture'>
-          <img src='../public/images/profile-pic.jpeg' alt='Alex Bilba' />
+          <img src='/images/profile-pic.jpeg' alt='Alex Bilba' />
         </div>
       </div>
       <div className='status-indicator'>
